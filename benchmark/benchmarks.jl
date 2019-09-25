3c0bfdae20eda689d6b38ac85679f9dfe47f3a92
Miles Frain <miles.frain@colorado.edu>
2019-09-24 18:45:08 -0700

    heap benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,25 +1,41 @@
+using Pkg
+tempdir = mktempdir()
+Pkg.activate(tempdir)
+Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
+Pkg.add(["BenchmarkTools", "PkgBenchmark", "Random"])
+Pkg.resolve()
+
+using DataStructures
 using BenchmarkTools
 using Random
 
+function push_heap(h::AbstractHeap, xs::Vector{Float64})
+    n = length(xs)
+
+    for i = 1 : n
+        push!(h, xs[i])
+    end
+end
+
+function pop_heap(h::AbstractHeap)
+    n = length(h)
+
+    for i = 1 : n
+        pop!(h)
+    end
+end
+
+Random.seed!(0)
+xs = rand(10^6)
+
 const SUITE = BenchmarkGroup()
 
-SUITE["utf8"] = BenchmarkGroup(["string", "unicode"])
-teststr = String(join(rand(MersenneTwister(1), 'a':'d', 10^4)))
-SUITE["utf8"]["replace"] = @benchmarkable replace($teststr, "a" => "b")
-SUITE["utf8"]["join"] = @benchmarkable join($teststr, $teststr)
-SUITE["utf8"]["plots"] = BenchmarkGroup()
-
-SUITE["trigonometry"] = BenchmarkGroup(["math", "triangles"])
-SUITE["trigonometry"]["circular"] = BenchmarkGroup()
-for f in (sin, cos, tan)
-    for x in (0.0, pi)
-        SUITE["trigonometry"]["circular"][string(f), x] = @benchmarkable ($f)($x)
-    end
-end
-
-SUITE["trigonometry"]["hyperbolic"] = BenchmarkGroup()
-for f in (sin, cos, tan)
-    for x in (0.0, pi)
-        SUITE["trigonometry"]["hyperbolic"][string(f), x] = @benchmarkable ($f)($x)
-    end
-end
+SUITE["heap"] = BenchmarkGroup(["binaryheap"])
+SUITE[["heap","basic", "min", "push"]] =
+    @benchmarkable push_heap(h, $xs) setup=(h=BinaryMinHeap{Float64}())
+SUITE[["heap","basic", "min", "pop"]] =
+    @benchmarkable pop_heap(h) setup=(h=BinaryMinHeap{Float64}($xs))
+SUITE[["heap","mutable", "min", "push"]] =
+    @benchmarkable push_heap(h, $xs) setup=(h=MutableBinaryMinHeap{Float64}())
+SUITE[["heap","mutable", "min", "pop"]] =
+    @benchmarkable pop_heap(h) setup=(h=MutableBinaryMinHeap{Float64}($xs))
